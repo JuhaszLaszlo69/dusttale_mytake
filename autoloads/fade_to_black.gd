23d65ef9f593a9182bbaa0b53af3d7d17a72cb8a
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2026-01-03 23:55:27 +0100

    Every collision is done, TODO: Door_SFX configuration + Y-Sorting, Z-index manipulation

--- autoloads/fade_to_black.gd
+++ autoloads/fade_to_black.gd
@@ -1,8 +1,17 @@
 extends CanvasLayer
 
-func fade_into_black() -> void:
-	%Anim.play("to_black")
-	await %Anim.animation_finished
+signal fade_finished
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var door_sfx: AudioStreamPlayer = $DoorSFX
+
+func fade_into_black(duration: float = 1) -> void:
+	anim.play("to_black")
+	anim.speed_scale = anim.current_animation_length / duration
+	await anim.animation_finished
 
 func fade_from_black() -> void:
-	%Anim.play("from_black")
+	anim.speed_scale = 1.0
+	anim.play("from_black")
+	await anim.animation_finished
+	emit_signal("fade_finished")
--- main_map.gd
+++ main_map.gd
@@ -1,5 +1,25 @@
 extends Node2D
 
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+@onready var door_sfx: AudioStreamPlayer = $DoorSFX
+
+
 func _ready():
 	Fade.fade_from_black()
+	Fade.fade_finished.connect(_on_fade_finished)
 
+func open_door():
+	# Load door sound
+	door_sfx.stream = load("res://addons/SMRT/Minecraft Door Sound Effect (Open and Close Sound Effects)..mp3")
+	door_sfx.play()
+	print("Door opened")
+
+	# Get sound length
+	var door_length := door_sfx.stream.get_length()
+
+	# Start fade synced to sound
+	await Fade.fade_into_black(door_length)
+
+func _on_fade_finished():
+	music_player.stream = load("res://addons/SMRT/005. Ruins (UNDERTALE Soundtrack) - Toby Fox.mp3")
+	music_player.play()
